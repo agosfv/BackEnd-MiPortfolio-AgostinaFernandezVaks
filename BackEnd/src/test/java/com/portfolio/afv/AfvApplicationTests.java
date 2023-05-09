f8e93c425a49a90d8c3493882a551c8423af4638
agosfv <agosvaks@gmail.com>
2023-05-09 20:51:41 -0300

    Creacion del BackEnd

---
+++ BackEnd/src/test/java/com/portfolio/afv/AfvApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.afv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AfvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
