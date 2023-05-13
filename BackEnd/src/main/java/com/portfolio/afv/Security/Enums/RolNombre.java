a6a89381a88e8f2f2042b09150ee4ace1e569a46
agosfv <agosvaks@gmail.com>
2023-05-13 00:29:17 -0300

    actualizacion backend

--- BackEnd/src/main/java/com/portfolio/afv/Security/Enums/RolNombre.java
+++ BackEnd/src/main/java/com/portfolio/afv/Security/Enums/RolNombre.java
@@ -1,5 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
 package com.portfolio.afv.Security.Enums;
 
-public class RolNombre {
-    
+/**
+ *
+ * @author usuario
+ */
+public enum RolNombre {
+    ROLE_ADMIN, ROLE_USER
 }
