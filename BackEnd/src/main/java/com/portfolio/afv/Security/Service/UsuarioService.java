22d9b194f6bf6aa21236e8bb0d7d49a7d7c32ffb
agosfv <agosvaks@gmail.com>
2023-05-15 23:03:09 -0300

    actualizacion backend

---
+++ BackEnd/src/main/java/com/portfolio/afv/Security/Service/UsuarioService.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.portfolio.afv.Security.Service;
+
+
+import com.portfolio.afv.Security.Entity.Usuario;
+import com.portfolio.afv.Security.Repository.iUsuarioRepository;
+import java.util.Optional;
+import javax.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+@Transactional
+public class UsuarioService {
+    @Autowired
+    iUsuarioRepository iusuarioRepository;
+    
+    public Optional<Usuario> getByNombreUsuario (String nombreUsuario){
+        return iusuarioRepository.findByNombreUsuario(nombreUsuario);
+    }
+    public boolean existByNombreUsuario(String nombreUsuario) {
+        return iusuarioRepository.existByNombreUsuario(nombreUsuario);
+    }
+        public boolean existByEmail(String email) {
+        return iusuarioRepository.existByEmail(email);
+    }
+        public void save(Usuario usuario){
+            iusuarioRepository.save(usuario);
+        }
+}
